e78bdf869f7fa45415c28bcaeeb83978814de5a6
James Hughes <jhughes@sci.utah.edu>
2014-01-29 20:25:01 -0700

    Switched uint64_t id to a string based ID for systems.

--- entity-system/BaseSystem.hpp
+++ entity-system/BaseSystem.hpp
@@ -14,7 +14,7 @@ public:
   virtual void walkComponents(ESCore& core) = 0;
   virtual bool walkEntity(ESCore& core, uint64_t entityID) = 0;
 
-  virtual uint64_t getUniqueID() {return 0;}
+  virtual const char* getID() {return nullptr;}
 };
 
 } // namespace CPM_ES_NS
